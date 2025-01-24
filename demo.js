6efa17d0b622b954b6e6bcd8785a996fb73fa576
joshna0105 <152901397+joshna0105@users.noreply.github.com>
2025-01-24 14:36:56 +0530

    added to repo

--- demo.js
+++ demo.js
@@ -1,7 +1,96 @@
 // document.getelementById("");
-let element = document.getElementById("demo");
-element.innerText = "Dom";
-console.log(element);
-let div = document.getElementById("test");
-div.innerHTML = "<h1>header</h1>"
-console.log(div);
+// let element = document.getElementById("demo");
+// element.innerText = "Dom";
+// console.log(element);
+// let div = document.getElementById("test");
+// div.innerHTML = "<h1>header</h1>"
+// console.log(div);
+
+// let ele = document.getElementsByClassName("test");
+// // console.log(ele);
+// // console.log(Array.isArray(ele));
+// ele[3].style.backgroundColor="yellow"
+
+// let x = [...ele];
+// console.log(x , Array.isArray(x));
+// x.map((element)=>{
+//     // console.log(element);
+//     element.style.backgroundColor="tea1";
+// })
+
+
+// let ele = document.querySelector("#demo");
+// // console.log(ele);
+
+// let ele = document.querySelector(".test");
+// console.log(ele);
+
+// let ele = document.querySelectorAll(".test");
+// console.log(ele);
+// [...ele].map(element=>{
+//     console.log(element.innerText);
+// })
+
+
+// ! document.getElementsByTagName()
+
+// let ele = document.getElementsByTagName("div");
+// console.log(ele);
+
+// ! document.getElementsByName()
+
+// let ele = document.getElementsByName("username");
+// console.log(ele);
+// [...ele].map(element=>{
+//     // console.log(element);
+//     element.style.backgroundColor="aqua"
+// })
+
+//!
+
+// let ele = document.querySelector("#demo");
+// console.log(ele);
+
+// let ele = document.querySelector(".test");
+// console.log(ele);
+
+
+// let ele=  document.querySelectorAll(".test");
+// // console.log(ele);
+// [...ele].map(element=>{
+//     console.log(element.innerText);
+// })
+
+// ! events
+// let btn = document.getElementById("btn");
+// // btn.addEventListener("events" , callbackfn)
+// btn.addEventListener("click",()=>{
+//     console.log("button clicked");
+//     document.body.style.background="dodgerblue"
+// })
+
+// let btn = document.getElementById("btn");
+// btn.addEventListener("dblclick",()=>{
+//     console.log("button is clicked 2 times");
+// })
+
+// let btn = document.getElementById("btn");
+// btn.addEventListener("mouseover" , ()=>{
+//     // alert("mouse over done")
+//     document.body.style.background="teal"
+// })
+
+// btn.addEventListener("mouseleave" , ()=>{
+//     // alert("mouse leave done")
+//     document.body.style.background="tomato"
+// })
+
+
+// let inp = document.querySelector("#inp");
+// inp.addEventListener("keydown",()=>{
+//     console.log("key down");
+// })
+
+// inp.addEventListener("keyup",()=>{
+//     console.log("key up");
+// })
