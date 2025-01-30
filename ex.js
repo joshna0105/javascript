c849e67fd889a2c84cb6902f02fc26e3c2f9fa8f
joshna0105 <152901397+joshna0105@users.noreply.github.com>
2025-01-30 21:40:20 +0530

    added to repo

---
+++ ex.js
@@ -0,0 +1,23 @@
+
+let employeeDetails=[
+    empid = 12,
+    empname = "navya",
+    empcompany ="xyx",
+    empsalary = 300000,
+    empaddress{
+        empcity = "hyd",
+        emparea = "huda colony",
+    }
+
+    empid = 123,
+    empname = "bhavya",
+    empcompany ="ayx",
+    empsalary = 3001000,
+    empaddress{
+        empcity = "nampallu",
+        emparea = "guda colony",
+    }
+]
+
+
+    console.log(employeeDetails);
